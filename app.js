c7f45e99effb22af086cec04144ef2378513e3d4
RumenRT <33309rt@gmail.com>
2018-11-13 16:13:57 -0500

    final

--- app.js
+++ app.js
@@ -1,58 +1,158 @@
-let voters = [];
-let democratCandidates = [];
-let republicanCandidates = [];
-let independentCandidates = [];
+var faker = require('faker');
 
-class Person {
-    constructor(name){
-        this.name = name;
+let voters = [],
+    democratCandidates = [],
+    republicanCandidates = [],
+    independentCandidates = [];
+
+class Person { 
+    constructor(name) {
+      this.name = name;
     }
 }
-
-class Voter extends Person{
-    constructor(name, ideology){
+class Voter extends Person {
+    constructor(name, ideology) {
         super(name);
         this.ideology = ideology;
     }
-    newVoter() {
-        voters.push(this.name)
-    }
 }
-class Candidate extends Person{
-    constructor(name, party){
+class Candidate extends Person {
+    constructor(name, party) {
         super(name);
         this.party = party;
-        this.votes = 0
-    }
-    newCandidate(){
-        if (this.party===democratCandidates) {
-            return democratCandidates.push(this.party)
-        }
-        else if (this.party===republicanCandidates) {
-            return republicanCandidates.push(this.party)
-        }
-        else {return independentCandidates.push(this.party)}
+        this.votes = 0;
     }
 }
 
-//Add an event listener for the submit event of the Voter form. The event handler for this event should take the values in both the form text input and dropdown and create a new Voter instance. Afterwards it must create an <li> DOM element with the following class "list-group-item". Then it must append said <li> to the <ul> in the voter list <div>.
+$('#voter-form form').submit(function(e) {
+        e.preventDefault();
+        let voterName = $('#voter-name').val();
+        let voterIdeology = $('#voter-ideology').val();
+        let voter = new Voter(voterName, voterIdeology);
+        voters.push(voter);
+        let li = $('<li></li>').text(`${voterName}, ${voterIdeology}`).addClass("list-group-item");
+       $('#voter-list ul').append(li);
+    }
+)
 
-$('#voter-form').submit((e) => {
+
+$('#candidate-form form').submit(function(e) {
     e.preventDefault();
-    
-    $.get('/users', function (data) {
-        data.forEach(el => {
-            let li = $('<li></li>').text(el.first_name);
-            $(li).addClass('person-li');
-            $('#people-list').append(li);
+    let candidateName = $('#candidate-name').val();
+    let candidateParty = $('#candidate-party').val();
+    let candidate = new Candidate(candidateName, candidateParty);
+    if(candidate.party === 'Democrat'){
+        democratCandidates.push(candidate);
+    }else if(candidate.party === 'Republican'){
+        republicanCandidates.push(candidate);
+    }else{
+        independentCandidates.push(candidate);
+    }
+    let li = $('<li></li>').text(`${candidateName}, ${candidateParty}`).addClass("list-group-item");
+   $('#candidate-list ul').append(li);
+  });
+    $('.btn.btn-danger').click(function(){
+      vote(voters);
+});
 
-            let name = $('#fetch-data-name').val();
-            let lastName = $('#fetch-data-lastName').val();
-            let email = $('#fetch-data-email').val();
-            let li = $('<li></li>').text(`${name}\n${lastName}\n${email}`);
-            $('#person-list').append(li);
+$('#randomize-btn').click(function(){
+    event.preventDefault();
+    while (voters.length < 100) {
+        let ideology = ['Conservative', 'Liberal', 'Neutral'];
+        let voterName = faker.name.findName();
+        let voterIdeology = ideology[Math.floor(Math.random()*ideology.length)];
+        let voter = new Voter(voterName, voterIdeology);
+        voters.push(voter);
+        let li = $('<li></li>').text(`${voterName}, ${voterIdeology}`).addClass("list-group-item");
+        $('#voter-list ul').append(li);
+    }
 })
 
+//Vote function selects a random candidate for the voter based on ideology as it relates to the outputs of partyVote and pickCandidate
 
+function vote(voters){
+    voters.forEach(function(v){
+        if(v.ideology === "Liberal"){
+            // console.log(partyVote(.6,.8));
+            return pickCandidate(partyVote(.6,.8));
+        } else if (v.ideology === "Neutral") {
+            // console.log(partyVote(.5,.75))
+            return pickCandidate(partyVote(.5,.75));
+        } else {
+            // console.log(partyVote(.2,.4))
+            return pickCandidate(partyVote(.2,.4));
+        }
+    })
+    pickWinner();
+}
 
+//partyVote function picks a party based on probability as it relates to voter ideaology
 
+function partyVote(a,b){
+    let num = Math.random();
+    if (num<a) {
+        return 'Democrat';
+    } else if (num<b) {
+        return 'Independent';
+    }else{
+        return 'Republican';
+    }
+}
+
+//pickCandidate function picks a random candidate from the voter's corresponding candidate array and adds 1 to the votes count
+
+function pickCandidate(party) {
+    console.log(party);
+    if (party === 'Democrat') {
+        let index = Math.floor(Math.random()*democratCandidates.length)
+        // console.log(index)
+        let pick = democratCandidates[index];
+        pick.votes ++;
+        // console.log(pick);
+    } else if (party === 'Republican') {
+        let index = Math.floor(Math.random()*republicanCandidates.length)
+        // console.log(index)
+        let pick = republicanCandidates[index];
+        pick.votes ++;
+        // console.log(pick);
+    } else {
+        let index = Math.floor(Math.random()*independentCandidates.length)
+        // console.log(index)
+        let pick = independentCandidates[index];
+        pick.votes ++;
+        // console.log(pick);
+    }
+}
+
+function pickWinner() {
+    let demWinner = democratCandidates.reduce(function(acc, current) {
+        if (acc.votes > current.votes) {
+            return acc;
+        } else {
+            return current;
+        }
+    })
+    let repWinner = republicanCandidates.reduce(function(acc, current) {
+        if (acc.votes > current.votes) {
+            return acc;
+        } else {
+            return current;
+        }
+    })
+    let indWinner = independentCandidates.reduce(function(acc, current) {
+        if (acc.votes > current.votes) {
+            return acc;
+        } else {
+            return current;
+        }
+    })
+    let winners = [demWinner, repWinner, indWinner];
+    let finalWinner = winners.reduce(function(acc, current) {
+        if (acc.votes > current.votes) {
+            return acc;
+        } else {
+            return current;
+        }
+    })
+    alert(finalWinner.name + " wins!")
+}
